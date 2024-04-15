fb3de3f0697d9071dabb377bd2fb83d5a6a2ed08
el glioui <elgliouiyoucode@gmail.com>
2024-04-15 05:49:57 +0200

    crete page 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,111 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<style>
+@include('includes.header')
 
-</style>
-    <!-- metas -->
-    <meta charset="utf-8">
-    <meta name="author" content="Chitrakoot Web" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta name="keywords" content="Agriculture Farming HTML Template" />
-    <meta name="description" content="Organic - Agriculture Farming HTML Template" />
-
-    <!-- title  -->
-    <title>Organic - Agriculture Farming HTML Template</title>
-
-    <!-- favicon -->
-    <link rel="shortcut icon" href="img/logos/favicon.png" />
-    <link rel="apple-touch-icon" href="img/logos/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="img/logos/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="img/logos/apple-touch-icon-114x114.png" />
-
-    <!-- plugins -->
-    <link rel="stylesheet" href="css/plugins.css" />
-
-    <!-- search css -->
-    <link rel="stylesheet" href="search/search.css" />
-
-    <!-- quform css -->
-    <link rel="stylesheet" href="quform/css/base.css">
-
-    <!-- core style css -->
-    <link href="css/styles.css" rel="stylesheet" />
-
-</head>
-
-<body>
-
-    <!-- PAGE LOADING
-    ================================================== -->
-    <div id="preloader"></div>
-
-    <!-- 404 PAGE
-    ================================================== -->
-    <div class="full-screen bg-img p-0" data-overlay-dark="2" data-background="img/bg/bg-04.jpg">
-        <div class="container d-flex flex-column">
-            <div class="row align-items-center justify-content-center text-center min-vh-100">
-                <div class="col-lg-10">
-                    <div class="error-wrapper p-1-9 p-sm-6 p-lg-10">
-                        <div class="error-text">
-                            <span>404</span>
-                        </div>
-                        <h3 class="display-20 display-xl-15 mb-xl-4 text-uppercase letter-spacing-4">Page Not Found</h3>
-                        <p class="mb-1-9 display-25 w-md-95 w-lg-85 mx-auto text-white">We can’t find the page your are looking for. You can check out our help center or head back to homepage.</p>
-                        <div class="mb-1-9 mb-lg-5">
-                            <a href="index.html" class="butn-style4 medium"><span>Back to Home</span></a>
-                        </div>
-                        <ul class="social-icon-style3">
-                            <li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#!"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#!"><i class="fab fa-youtube"></i></a></li>
-                            <li><a href="#!"><i class="fab fa-linkedin-in"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- BUY TEMPLATE
-    ================================================== -->
-    <div class="buy-theme alt-font d-none d-lg-inline-block"><a href="https://wrapbootstrap.com/theme/organic-agriculture-farming-html-template-WB090D201" target="_blank"><i class="fas fa-cart-plus"></i><span>Buy Template</span></a></div>
-
-    <div class="all-demo alt-font d-none d-lg-inline-block"><a href="https://www.chitrakootweb.com/contact.html" target="_blank"><i class="far fa-envelope"></i><span>Quick Question?</span></a></div>
-
-    <!-- start scroll to top -->
-    <a href="#!" class="scroll-to-top"><i class="fas fa-angle-up" aria-hidden="true"></i></a>
-    <!-- end scroll to top -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.min.js"></script>
-
-    <!-- popper js -->
-    <script src="js/popper.min.js"></script>
-
-    <!-- bootstrap -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- core.min.js -->
-    <script src="js/core.min.js"></script>
-
-    <!-- search -->
-    <script src="search/search.js"></script>
-
-    <!-- custom scripts -->
-    <script src="js/main.js"></script>
-
-    <!-- form plugins js -->
-    <script src="quform/js/plugins.js"></script>
-
-    <!-- form scripts js -->
-    <script src="quform/js/scripts.js"></script>
-
-    <!-- all js include end -->
-    
-</body>
-
-
-<!-- Mirrored from organic.websitelayout.net/404.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 08 Apr 2024 12:16:51 GMT -->
-</html>
+<div class="flex items-center justify-center min-h-screen bg-cover bg-center bg-no-repeat"
+  style="background-image: url('{{ asset('img/vache.jpg') }}')">
+  <div class="max-w-md mx-auto text-center  bg-white bg-opacity-90 p-8 rounded-lg shadow-lg">
+    <div class="text-9xl font-bold text-green-500 mb-4">404</div>
+    <h1 class="text-4xl font-bold text-gray-800 mb-6">Oops ! Page non trouvée</h1>
+    <p class="text-lg text-gray-600 mb-8">La page que vous recherchez semble avoir pris un petit chemin. Ne vous inquiétez pas, nous allons vous aider à retrouver votre chemin vers la maison.</p>
+    <a href="#"
+      class="inline-block bg-green-500 text-green font-semibold px-6 py-3 rounded-md hover:bg-indigo-700 transition-colors duration-300">
+      Retourner à la page d'accueil
+    </a>
+  </div>
+</div>
