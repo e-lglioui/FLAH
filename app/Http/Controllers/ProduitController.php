697a3c0debb332dc12be689bda4fd18982184b80
el glioui <elgliouiyoucode@gmail.com>
2024-04-21 14:33:35 +0200

    changer le crud de oroduit

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produit;
+use App\Models\Image;
 use App\Services\ProduitService;
 use App\Http\Requests\StoreProduitRequest;
 use App\Http\Requests\UpdateProduitRequest;
@@ -22,12 +23,12 @@ class ProduitController extends Controller
     public function index()
     {
         $produit = $this->ProduitService->getAllProduits();
-        return response()->json($produit);
+        return view('home', compact('produit'));
     }
 
     public function create()
     {
-        //
+        return view('fornissuer.produit-create');
     }
 
  
@@ -36,7 +37,6 @@ class ProduitController extends Controller
         if (auth()->check()) {
             $requestData = $request->all();
             $requestData['forniseur_id'] = auth()->user()->id;
-    
             $validator = Validator::make($requestData, [
                 'nom' => 'required',
                 'description' => 'required',
@@ -44,31 +44,40 @@ class ProduitController extends Controller
                 'quantite' => 'required',
                 'category_id' => 'required',
                 'forniseur_id' => 'required',
+                'images' => 'nullable|array', //liste des image
+                'images.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048', //valider les images
             ]);
     
             if ($validator->fails()) {
-                return response()->json(['error' => $validator->errors()], 422);
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+            $produit = $this->ProduitService->create($requestData);
+            if ($request->hasFile('images')) {
+                foreach ($request->file('images') as $image) {
+                    $path = $image->store('images/produits', 'public'); // Stockage de l'image
+                    $produit->images()->create(['chemin' => $path]); // Associer l'image au produit
+                }
             }
     
-            $Produit = $this->ProduitService->create($requestData);
-    
-            return response()->json($Produit, 201);
+            return redirect()->route('categorie.index')->with('success', 'Produit créé avec succès');
         } else {
-            
             return response()->json(['error' => 'Unauthorized'], 401);
         }
     }
+    
 
   
     public function show(Produit $produit)
     {
-        //
+        $images = $produit->images; 
+        return view('produit.show', compact('produit', 'images'));
     }
+    
 
   
     public function edit(Produit $produit)
     {
-        //
+        return view('fornissuer.produit-edit');
     }
 
    
@@ -80,6 +89,14 @@ class ProduitController extends Controller
 
     public function destroy(Produit $produit)
     {
-        //
+        foreach ($produit->images as $image) {
+            Storage::disk('public')->delete($image->chemin); 
+            $image->delete(); 
+        }
+    
+        $produit->delete(); 
+    
+        return redirect()->route('produit.index')->with('success', 'Produit supprimé avec succès');
     }
+    
 }
