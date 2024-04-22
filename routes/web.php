5a1759ebf8813bc8a4fda6ace1df2a633b42bd82
el glioui <elgliouiyoucode@gmail.com>
2024-04-22 11:48:28 +0200

    faire la validation cote clien de login et register et fixe les meddlware

--- routes/web.php
+++ routes/web.php
@@ -23,21 +23,40 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::resource('admin/categorie', CategorieController::class);
-Route::get('/admin',[StatistiqueController::class,'index'])->name('admin');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 Route::get('/login', [AuthController::class,'login'])->name('login');
 Route::get('/register', [AuthController::class,'register'])->name('register');
 Route::post('/register', [AuthController::class, 'signup'])->name('signup');
 Route::post('/login', [AuthController::class,'singin'])->name('singin');
 Route::get('/contact', [DemandeController::class,'contact'])->name('contact');
 Route::get('/contact', [DemandeController::class,'contact'])->name('contact');
-Route::get('/admin/demande', [DemandeController::class,'index'])->name('demande');
-Route::delete('/admin/demande/{id}', [DemandeController::class, 'destroy'])->name('destroy');
-Route::get('/admin/demande/{id}', [DemandeController::class,'veterinaire'])->name('veterinaire');
-Route::get('/admin/demande/{id}', [DemandeController::class,'fornissuer'])->name('fornissuer');
-// Route::get('/logout/success', function () {
-//     return view('logout');
-// })->name('logout.success');
 
-Route::resource('fornisseur/produit', ProduitController::class);
-Route::get('/fornisseur',[StatistiqueController::class,'fornissuer'])->name('fornissuerStatistique');
+Route::get('/logout/success', function () {
+    return view('logout');
+})->name('logout.success');
+
+
+
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin',[StatistiqueController::class,'index'])->name('admin');
+    Route::resource('admin/categorie', CategorieController::class);
+    Route::get('/admin/demande', [DemandeController::class,'index'])->name('demande');
+    Route::delete('/admin/demande/{id}', [DemandeController::class, 'destroy'])->name('destroy');
+    Route::get('/admin/demande/{id}', [DemandeController::class,'veterinaire'])->name('veterinaire');
+    Route::get('/admin/demande/{id}', [DemandeController::class,'fornissuer'])->name('fornissuer');  
+});
+
+
+Route::middleware(['auth', 'fornissuer'])->group(function () {
+    Route::get('/fornisseur', [StatistiqueController::class, 'fornissuer'])
+        ->name('fornissuerStatistique'); 
+    Route::resource('fornisseur/produit', ProduitController::class);
+});
+
+Route::fallback(function () {
+    return response()->view('404', [], 404); 
+});
