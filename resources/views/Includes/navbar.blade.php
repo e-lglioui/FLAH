cd35a8f2c159a77c3afde35d4a81a19595516cc6
el glioui <elgliouiyoucode@gmail.com>
2024-04-22 16:15:54 +0200

    les categorie

--- resources/views/Includes/navbar.blade.php
+++ resources/views/Includes/navbar.blade.php
@@ -1,5 +1,5 @@
-<nav class="bg-white border-gray-200 dark:bg-gray-900 h-20" >
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+<nav class="bg-white border-gray-200 dark:bg-gray-900 h-20  " >
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-around mx-auto p-4">
       <a href="home" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="{{ asset('img/Flah.png') }}" class="h-20" alt="Logo" />
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white  text-green-500 ">Flah</span>
---
+++ resources/views/top6Categorie.blade.php
@@ -0,0 +1,9 @@
+<div class="bg-green-700 border-gray-200 dark:bg-gray-900 h-20  top-0 w-full z-10"> <!-- Fixe la barre de navigation en haut -->
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-around " >
+        <a href="home" class=" text-white text-center p-10" aria-current="page">
+            Nouveaux produits
+        </a>
+        
+    </div>
+</div>
+
